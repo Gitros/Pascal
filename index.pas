7451972a68ff93d0fe786ddc4811d07c8aaa860e
Gitros <31899790+Gitros@users.noreply.github.com>
2023-12-09 10:23:01 +0100

    pascal

---
+++ index.pas
@@ -0,0 +1,30 @@
+program Liczby;
+
+var a: char;
+
+begin
+    read(a);
+
+    if a = 'a' then 
+        writeln('samogloska') 
+    
+    else if a = 'e' then 
+        writeln('samogloska') 
+        
+    else if a = 'i' then 
+        writeln('samogloska') 
+        
+    else if a = 'o' then 
+        writeln('samogloska') 
+        
+    else if a = 'u' then 
+        writeln('samogloska')
+ 
+    else if a = 'y' then 
+        writeln('samogloska') 
+        
+    else
+        writeln('wspolgloska');
+        
+    readln;
+end.
